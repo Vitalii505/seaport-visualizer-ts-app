52d278044cd6465a4b4fce89bf8fb0d6670edb8e
Vitalii <63597786+Vitalii505@users.noreply.github.com>
2023-12-11 15:17:46 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-// import { GameApp } from './1...test/src/app/app';
-
 import { PortSimulationApp } from "./core/PortSimulationApp";
 
-
 window.onload = () => {
-    // const gameAppRender = new GameApp(600, 500);
-    // gameAppRender.start();
-
     const gameAppRender = new PortSimulationApp(800, 500);
     gameAppRender.start();
 };
